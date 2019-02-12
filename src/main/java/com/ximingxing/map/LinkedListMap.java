33463dbcbaa2714a9d1e543a237f75fbead82b27
xxm <ximingxing@gmail.com>
2019-02-12 23:36:32 +0800

    linkedlist map and some method

---
+++ src/main/java/com/ximingxing/map/LinkedListMap.java
@@ -0,0 +1,88 @@
+package com.ximingxing.map;
+
+/**
+ * Description:
+ * Created By xxm
+ */
+public class LinkedListMap<K, V> implements Map<K, V> {
+
+    private class Node {
+        K key;
+        V value;
+        Node next;
+
+        Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        Node(K key) {
+            this(key, null, null);
+        }
+
+        Node() {
+            this(null, null, null);
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedListMap() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    private Node getNode(K key) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.key.equals(key)) {
+                return cur;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void add(K key, V value) {
+        Node node = getNode(key);
+        if (node == null) {
+            dummyHead.next = new Node(key, value, dummyHead.next);
+            size++;
+        } else {
+            node.value = value;
+        }
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
---
+++ src/main/java/com/ximingxing/map/Map.java
@@ -0,0 +1,21 @@
+package com.ximingxing.map;
+
+/**
+ * Description: Map interface
+ * Created By xxm
+ */
+public interface Map<K, V> {
+    void add(K key, V value);
+
+    V remove(K key);
+
+    boolean contains(K key);
+
+    V get(K key);
+
+    void set(K key, V newValue);
+
+    int getSize();
+
+    boolean isEmpty();
+}
