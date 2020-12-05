82ba1b1cd8e8e4ecf9b1c09d0715d73d3ffa43f6
ximing <ximingxing@gmail.com>
2020-12-05 17:58:44 +0800

    #88 Merge Sorted Array update

--- 88-Merge-Sorted-Array/src/Solution.java
+++ 88-Merge-Sorted-Array/src/Solution.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -5,6 +6,8 @@ import java.util.Arrays;
  * <p>
  * Runtime: 0ms
  * Memory : 39.1MB
+ * Time Complexity  : O((m+n)log(m+n))
+ * Space Complexity : O(1)
  * <p>
  * Created By xxm
  */
@@ -12,21 +15,23 @@ public class Solution {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int j = 0;
         for (int i = 0; i < nums1.length; i++) {
+            // merge num1 and num2, start from zero value in num1
             if (nums1[i] == 0 && j != nums2.length) {
                 nums1[i] = nums2[j++];
             }
         }
         Arrays.sort(nums1);
-//        System.out.println(nums1);
     }
 
     public static void main(String[] args) {
         int a[] = new int[]{1, 2, 3, 0, 0, 0};
         int b[] = new int[]{2, 5, 6};
         merge(a, a.length + b.length, b, b.length);
+        System.out.println(Arrays.toString(a));
 
         int c[] = new int[]{0};
         int d[] = new int[]{1};
         merge(c, 0, d, 1);
+        System.out.println(Arrays.toString(c));
     }
 }
--- 88-Merge-Sorted-Array/src/Solution2.java
+++ 88-Merge-Sorted-Array/src/Solution2.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 public class Solution2 {
     public static void merge(int[] num1, int m, int[] num2, int n) {
         while (n > 0) num1[m + n - 1] = (m == 0 || num2[n - 1] > num1[m - 1]) ? num2[--n] : num1[--m];
-//        System.out.println(Arrays.toString(num1));
     }
 
     public static void main(String[] args) {
