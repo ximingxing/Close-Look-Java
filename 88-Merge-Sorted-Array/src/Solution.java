b579f9525ec507f95ed396dc2236b3a024fad469
ximing <ximingxing@gmail.com>
2020-12-05 11:34:28 +0800

    #88 Merge Sorted Array update

--- 88-Merge-Sorted-Array/src/Solution.java
+++ 88-Merge-Sorted-Array/src/Solution.java
@@ -2,30 +2,31 @@ import java.util.Arrays;
 
 /**
  * Description: 88. Merge Sorted Array
+ * <p>
+ * Runtime: 0ms
+ * Memory : 39.1MB
+ * <p>
  * Created By xxm
  */
 public class Solution {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (m == 1 && n == 1) {
-            nums1 = new int[]{nums1[0], nums2[0]};
-            Arrays.sort(nums1);
-            System.out.println(Arrays.toString(nums1));
-            return;
-        }
-        for (int i = 0; i < nums2.length; i++) {
-            nums1[i + nums2.length] = nums2[i];
+        int j = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] == 0 && j != nums2.length) {
+                nums1[i] = nums2[j++];
+            }
         }
         Arrays.sort(nums1);
-        System.out.println(Arrays.toString(nums1));
+//        System.out.println(nums1);
     }
 
     public static void main(String[] args) {
-//        int a[] = new int[]{1, 2, 3, 0, 0, 0};
-//        int b[] = new int[]{2, 5, 6};
-//        merge(a, a.length, b, b.length);
+        int a[] = new int[]{1, 2, 3, 0, 0, 0};
+        int b[] = new int[]{2, 5, 6};
+        merge(a, a.length + b.length, b, b.length);
 
         int c[] = new int[]{0};
         int d[] = new int[]{1};
-        merge(c, c.length, d, d.length);
+        merge(c, 0, d, 1);
     }
 }
--- 88-Merge-Sorted-Array/src/Solution2.java
+++ 88-Merge-Sorted-Array/src/Solution2.java
@@ -2,22 +2,25 @@ import java.util.Arrays;
 
 /**
  * Description: one line solution
+ * <p>
+ * Runtime: 0ms
+ * Memory : 39.4MB
+ * <p>
  * Created By xxm
  */
 public class Solution2 {
-    public static void merge(int[] A, int m, int[] B, int n) {
-        while (n > 0) A[m + n - 1] = (m == 0 || B[n - 1] > A[m - 1]) ? B[--n] : A[--m];
-        System.out.println(Arrays.toString(A));
-        System.out.println(Arrays.toString(B));
+    public static void merge(int[] num1, int m, int[] num2, int n) {
+        while (n > 0) num1[m + n - 1] = (m == 0 || num2[n - 1] > num1[m - 1]) ? num2[--n] : num1[--m];
+//        System.out.println(Arrays.toString(num1));
     }
 
     public static void main(String[] args) {
         int a[] = new int[]{1, 2, 3, 0, 0, 0};
         int b[] = new int[]{2, 5, 6};
-        merge(a, a.length - b.length, b, b.length);
+        merge(a, 3, b, 3);
 
         int c[] = new int[]{0};
         int d[] = new int[]{1};
-        merge(c, c.length - d.length, d, d.length);
+        merge(c, 0, d, 1);
     }
 }
