fbdbbfdd90ae4aef36188cf0f8fcc0760a7b8844
Ximing Xing <ximingxing@gmail.com>
2021-03-19 15:26:31 +0800

    (new) #92

--- 92-Reverse-Linked-List-II/src/Solution.java
+++ 92-Reverse-Linked-List-II/src/Solution.java
@@ -1,55 +1,51 @@
 /**
  * Description:
+ * <p>
+ * 将需要翻转的链表截取出来单独翻转
+ * 再将翻转后的链表拼接回原链表之中
+ * <p>
  * Created By xxm
  */
 public class Solution {
     public static ListNode reverseBetween(ListNode head, int left, int right) {
         if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0, head);
 
-        // 链表下标
-        int n = 1;
-        // 需要翻转的节点数量
-        int len = right - left + 1;
-        // 待翻转的链表的头和尾
-        ListNode start = null, end = null;
-        // 不需要翻转的链表的头和尾
-        ListNode rawHead = head, rawTail = null;
+        // pre指向待翻转链表的前一个节点
+        ListNode pre = dummy;
+        for (int i = 0; i < left - 1; i++)
+            pre = pre.next;
 
-        // 查找
-        for (ListNode temp = head; temp.next != null && n <= right; temp = temp.next, n++) {
-            if (n == left) {
-                rawHead = temp;
-                rawHead.next = null;
-                start = temp;
-            }
-            if (n == right) {
-                end = temp.next;
-            }
-        }
+        // nRight指向待翻转链表的尾节点
+        ListNode nRight = pre;
+        for (int i = 0; i < right - left + 1; i++)
+            nRight = nRight.next;
 
-        // 翻转
+        // nLeft指向待翻转链表的头节点, newHead指向原链表剩余的表尾
+        ListNode nLeft = pre.next;
+        ListNode newHead = nRight.next;
+
+        // 将待翻转链表从原链表中截取
+        pre.next = null;
+        nRight.next = null;
+
+        // 翻转链表(这里是指截取出来的的子链表)
         ListNode r = null;
-        for (int i = 1; start != null && i <= len; i++) {
-            ListNode t = start;
-            start = start.next;
-            t.next = r;
-            r = t;
+        ListNode cur = nLeft;
+        while (cur != null) {
+            ListNode temp = cur;
+            cur = cur.next;
+            temp.next = r;
+            r = temp;
         }
 
-        // 拼接
-        ListNode temp = r;
-        while (temp != null) {
-            if (temp.next == null) {
-                temp.next = end;
-                break;
-            }
-            temp = temp.next;
-        }
+        // 将翻转后的链表拼接回原链表
+        pre.next = r;
+        nLeft.next = newHead;
 
-        return r;
+        return dummy.next;
     }
 
-    //
     public static void main(String[] args) {
         ListNode e = new ListNode(5);
         ListNode d = new ListNode(4, e);
@@ -63,7 +59,6 @@ public class Solution {
         print(newHead);
     }
 
-    //
     public static void print(ListNode head) {
         StringBuilder r = new StringBuilder();
         while (head != null) {
