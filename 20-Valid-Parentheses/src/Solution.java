a90fd49d6a2c913a7f86977494e69b5abac0612a
ximing Xing <ximingxing@gmail.com>
2021-03-20 09:58:40 +0800

    (new) #20

---
+++ 20-Valid-Parentheses/src/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+/**
+ * Description: 使用堆栈进行括号匹配.
+ */
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
