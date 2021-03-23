dc824ce58dc47fae0bc68c53f17dfbd782b910b0
Ximing Xing <ximingxing@gmail.com>
2021-03-23 22:40:30 +0800

    (new) #145

--- 145-Binary-Tree-Postorder-Traversal/src/Solution.java
+++ 145-Binary-Tree-Postorder-Traversal/src/Solution.java
@@ -1,5 +1,25 @@
-package PACKAGE_NAME;/**
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
  * Description:
+ * 后序遍历即为前序遍历的翻转
+ * 根据这个思路: 我们前序遍历二叉树, 给返回List头部添加结果即可.
  * Created By xxm
- */public class Solution {
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        for (stack.push(root); !stack.isEmpty(); ) {
+            root = stack.pop();
+            if (root != null) {
+                res.addFirst(root.val);
+                stack.push(root.left);
+                stack.push(root.right);
+            }
+        }
+        return res;
+    }
 }
--- 145-Binary-Tree-Postorder-Traversal/src/Solution2.java
+++ 145-Binary-Tree-Postorder-Traversal/src/Solution2.java
@@ -1,5 +1,41 @@
-package PACKAGE_NAME;/**
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
  * Description:
  * Created By xxm
- */public class Solution2 {
+ */
+public class Solution2 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // Pair<节点, 访问标记>
+        // 0: 左右子树都未访问
+        // 1: 访问过左子树, 未访问过右子树
+        // 2: 访问过右子树
+        Deque<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        for (stack.push(new Pair<>(root, 0)); !stack.isEmpty(); ) {
+            Pair<TreeNode, Integer> top = stack.pop();
+            root = top.getKey();
+            if (root != null) {
+                switch (top.getValue()) {
+                    case 0:
+                        stack.push(new Pair<>(root, 1));
+                        stack.push(new Pair<>(root.left, 0));
+                        break;
+                    case 1:
+                        stack.push(new Pair<>(root, 2));
+                        stack.push(new Pair<>(root.right, 0));
+                        break;
+                    case 2:
+                        res.add(root.val);
+                        break;
+                }
+            }
+        }
+        return res;
+    }
 }
