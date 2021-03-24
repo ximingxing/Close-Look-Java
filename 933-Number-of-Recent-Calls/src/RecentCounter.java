9bbd995bfe6e1631ef112fc5b15807344bbac102
Ximing Xing <ximingxing@gmail.com>
2021-03-24 21:50:33 +0800

    (new) #933

---
+++ 933-Number-of-Recent-Calls/src/RecentCounter.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Description:
+ * Created By xxm
+ */
+public class RecentCounter {
+    Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        q.add(t);
+        for (; !q.isEmpty() && q.peek() < t - 3000; q.remove()) ;
+        return q.size();
+    }
+}
+
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
