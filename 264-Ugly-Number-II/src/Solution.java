d45a0bca9744c9cb0d61fa5d75dd426ec813a2f0
Ximing Xing <ximingxing@gmail.com>
2021-04-11 15:18:09 +0800

    (fix) #264 add some annotations.

--- 264-Ugly-Number-II/src/Solution.java
+++ 264-Ugly-Number-II/src/Solution.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -7,11 +6,13 @@ import java.util.Arrays;
  */
 class Solution {
     public int nthUglyNumber(int n) {
-        // 如果 n 为1, 丑数序列就是它本身
+        // 如果 n 为1, 丑数对应就是它本身
         if (n == 1) return 1;
 
-        // 根据丑数的定义, 丑数: 就是只包含质因数 2、3 和/或 5 的正整数;
-        // 从 1 开始寻找丑数序列
+        // 根据丑数的定义,
+        // 丑数: 就是只包含质因数 2、3 和/或 5 的正整数;
+        // 定义三个指针: 表示下一个丑数是当前丑数乘以对应的质因数
+        // 有资格同 i 相乘的最小丑数的位置
         int i2 = 1, i3 = 1, i5 = 1;
         int[] ans = new int[n + 1];
         ans[1] = 1;
@@ -24,7 +25,7 @@ class Solution {
             ans[i] = ugly;
 
             // 如果当前 i 中包含已经求出的丑数, 则移动指针向前
-            // 避免重复判断
+            // 避免重复计算
             if (ans[i] == ans[i2] * 2) i2++;
             if (ans[i] == ans[i3] * 3) i3++;
             if (ans[i] == ans[i5] * 5) i5++;
