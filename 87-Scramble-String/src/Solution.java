591d1cb24de287fba5daf3bc90e85834ca2e293a
Ximing Xing <ximingxing@gmail.com>
2021-04-17 12:37:10 +0800

    (new) #87

--- 87-Scramble-String/src/Solution.java
+++ 87-Scramble-String/src/Solution.java
@@ -11,15 +11,16 @@ class Solution {
     int[][][] memo;
     String s1, s2;
 
+    /*  */
     public boolean isScramble(String s1, String s2) {
-        int length = s1.length();
-        this.memo = new int[length][length][length + 1];
+        int len = s1.length();
+        this.memo = new int[len][len][len + 1];
         this.s1 = s1;
         this.s2 = s2;
-        return dfs(0, 0, length);
+        return dfs(0, 0, len);
     }
 
-    // 第一个字符串从 i1 开始，第二个字符串从 i2 开始，子串的长度为 length，是否和谐
+    /* 第一个字符串从 i1 开始，第二个字符串从 i2 开始，子串的长度为 length，是否和谐 */
     public boolean dfs(int i1, int i2, int length) {
         if (memo[i1][i2][length] != 0) {
             return memo[i1][i2][length] == 1;
@@ -55,8 +56,9 @@ class Solution {
         return false;
     }
 
+    /* 读博士了 */
     public boolean checkIfSimilar(int i1, int i2, int length) {
-        Map<Character, Integer> freq = new HashMap<Character, Integer>();
+        Map<Character, Integer> freq = new HashMap<>();
         for (int i = i1; i < i1 + length; ++i) {
             char c = s1.charAt(i);
             freq.put(c, freq.getOrDefault(c, 0) + 1);
--- README.md
+++ README.md
@@ -173,6 +173,7 @@ With your questions, comments or even if you just want to say Hi.
 
 * `#53`   [Maximum Subarray](https://github.com/ximingxing/LeetCode-notepad/tree/master/53-Maximum-Subarray/src) <img src="https://img.shields.io/badge/-easy-green">
 * `#72`   [Edit Distance](https://github.com/ximingxing/LeetCode-notepad/tree/master/72-Edit-Distance/src) <img src="https://img.shields.io/badge/-hard-red">
+* `#87`   [Scramble String](https://github.com/ximingxing/LeetCode-notepad/tree/master/87-Scramble-String/src) <img src="https://img.shields.io/badge/-hard-red">
 * `#213`  [House Robber II](https://github.com/ximingxing/LeetCode-notepad/tree/master/213-House-Robber-II/src) <img src="https://img.shields.io/badge/-medium-yellow">
 * `#300`  [Longest Increasing Subsequence](https://github.com/ximingxing/LeetCode-notepad/tree/master/300-Longest-Increasing-Subsequence/src) <img src="https://img.shields.io/badge/-medium-yellow">
 * `#322`  [Coin Change](https://github.com/ximingxing/LeetCode-notepad/tree/master/322-Coin-Change/src) <img src="https://img.shields.io/badge/-medium-yellow">
