78e9696b53289338e4a8cbd20be487065bf1a2a5
Ximing Xing <ximingxing@gmail.com>
2021-04-26 17:06:52 +0800

    feat(#55): | Desc: jump game, Solution: greedy

---
+++ Code/55-Jump-Game/src/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * Description: 跳跃游戏(Jump Game) -- Greedy.md
+ * Solution: Greedy
+ * Created By xxm
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+        final int N = nums.length;
+        int farthest = 0;
+
+        // 每一步都计算从当前步最远能跳跃的位置`step + nums[step]`,
+        // 然后和一个全局最优位置`farthest`做对比,
+        // 通过每一步的最优解, 更新全局最优解.
+        for (int step = 0; step < N; step++) {
+            farthest = Math.max(farthest, step + nums[step]);
+            if (farthest < step) return false;
+        }
+
+        return true;
+    }
+}
