f0b82417b1f6288d94957bc61f4ce6f86a2e52b1
Ximing Xing <ximingxing@gmail.com>
2021-09-07 17:07:22 +0800

    feat(#714): desc: stock, Solution: DP

--- Code/714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee/src/Solution.java
+++ Code/714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee/src/Solution.java
@@ -1,5 +1,25 @@
-package PACKAGE_NAME;/**
- * Description:
+/**
+ * Description: 买卖多次一只股票获得最大利润，但每次卖出之后需要支付手续费.
+ * Solution: 状态机
  * Created By xingximing.xxm
- */public class Solution {
+ */
+public class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        final int N = prices.length;
+
+        int dp_i_0 = 0, dp_i_1 = -prices[0];
+
+        for (int i = 0; i < N; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i] - fee);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+
+        return dp_i_0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 2, 8, 4, 9};
+        System.out.println(new Solution().maxProfit(arr, 2));
+    }
 }
