9a90c4585473136443b743ab02d897b8176201ad
Ximing Xing <ximingxing@gmail.com>
2021-09-07 17:02:54 +0800

    feat(#122): desc: stock, Solution: DP

--- Code/122-Best-Time-to-Buy-and-Sell-Stock-II/src/Solution.java
+++ Code/122-Best-Time-to-Buy-and-Sell-Stock-II/src/Solution.java
@@ -1,5 +1,22 @@
-package PACKAGE_NAME;/**
- * Description:
+/**
+ * Description: 买卖多次一只股票获得最大利润
+ * Solution: 贪心法
  * Created By xingximing.xxm
- */public class Solution {
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        final int N = prices.length;
+        int maxProfit = 0;
+        for (int i = 0; i < N; i++) {
+            if (i + 1 < N && prices[i] < prices[i + 1]) {
+                maxProfit += prices[i + 1] - prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 1, 5, 6};
+        System.out.println(new Solution().maxProfit(arr));
+    }
 }
--- Code/122-Best-Time-to-Buy-and-Sell-Stock-II/src/Solution2.java
+++ Code/122-Best-Time-to-Buy-and-Sell-Stock-II/src/Solution2.java
@@ -1,22 +1,30 @@
 /**
  * Description: 买卖多次一只股票获得最大利润
- * Solution: 贪心法
+ * Solution: 状态机
  * Created By xingximing.xxm
  */
-public class Solution {
+public class Solution2 {
     public int maxProfit(int[] prices) {
         final int N = prices.length;
-        int maxProfit = 0;
+
+        // base case
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+
         for (int i = 0; i < N; i++) {
-            if (i + 1 < N && prices[i] < prices[i + 1]) {
-                maxProfit += prices[i + 1] - prices[i];
-            }
+            int temp = dp_i_0;
+            // dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k-1][1] + prices[1])
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            // dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[1])
+            //             = max(dp[i-1][k][1], dp[i-1][k][0] - prices[1])
+            // 由于 k = k-1 = inf
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
         }
-        return maxProfit;
+
+        return dp_i_0;
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{7, 1, 5, 6};
-        System.out.println(new Solution().maxProfit(arr));
+        System.out.println(new Solution2().maxProfit(arr));
     }
 }
