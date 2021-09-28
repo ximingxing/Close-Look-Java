6ab4912c5a6b4fa5b029d618211b196c7a596951
Ximing Xing <ximingxing@gmail.com>
2021-09-28 15:24:25 +0800

    feat(#72): |Desc: edit distance, Solution: DP

--- Code/72-Edit-Distance/src/Solution2.java
+++ Code/72-Edit-Distance/src/Solution2.java
@@ -1,10 +1,7 @@
-import java.util.Arrays;
 
 /**
- * Description:
- * <p>
- * 自底向上的动态规划
- * <p>
+ * Description: 编辑距离
+ * Solution: 动态规划
  * Created By xxm
  */
 public class Solution2 {
@@ -17,9 +14,15 @@ public class Solution2 {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
 
+        // 如果 word1 或者 word2 其中某个为空串
+        if (m * n == 0) return m + n;
+
+        // dp数组含义:
+        // word1 的前 i 个字符变为 word2 的前 j 个字符的编辑距离
         int[][] dp = new int[m + 1][n + 1];
 
-        // 初始化 dp table
+        // base case：
+        // 一个空串变为非空串需要的编辑距离
         for (int i = 1; i <= m; i++) {
             dp[i][0] = i;
         }
@@ -29,17 +32,17 @@ public class Solution2 {
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                // 状态转移方程:
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    // 如果 word1 和 word2 最后一个字符相同
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
+                    // 如果不同，则计算"插入"、"删除"、"替换"三种方式中最少的编辑距离
+                    // 1. word2 最后一个字符插入 word1 尾部
+                    // 2. 删除 word1 最后一个字符
+                    // 3. 将 word1 和 word2 最后一个字符替换
                     dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1;
                 }
             }
-        }
-
-        for (int[] ints : dp) {
-            System.out.println(Arrays.toString(ints));
         }
 
         return dp[m][n];
